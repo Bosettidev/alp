3a68a65f19d7c4ebeaf31a8425c73e66fdb1b4f1
João Vitor Bosetti <joao.b2009@aluno.ifsc.edu.br>
2025-02-07 08:00:01 -0300

    Add files via upload

---
+++ op.c
@@ -0,0 +1,9 @@
+#include <stdio.h>/*entrada saida*/
+int main(void){
+    int num = 1;
+    while (num != 11){
+        printf("%d \n",num);
+        num++;
+    }
+    return num;
+}
---
+++ readme.txt
@@ -0,0 +1,7 @@
+      C
+#include <studio.h>  entrada saida
+#include <conio.h>  caracteres
+#include <stdlib.h> funcoes de sistema
+return 0
+---
+
